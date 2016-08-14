c28ba7c24e5749ef6bb6af150a4e497c629d0597
jerryyongbusson <jerryyongbusson@gmail.com>
2016-08-14 18:28:33 +0200

    nice

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,24 +1,50 @@
 angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope,$cordovaImagePicker,$ionicPlatform) {
+  
   $scope.getImage = function(){
+        $scope.data= {image:""};
         $ionicPlatform.ready(function() {
           var options = {
            maximumImagesCount: 1,
-           width: 400,
-           height: 400,
-           quality: 80
+           width: 200,
+           height: 0,
+           quality: 50
           };
           $cordovaImagePicker.getPictures(options)
             .then(function (results) {
               for (var i = 0; i < results.length; i++) {
-                console.log(results[i]);
-                $scope.image=results[i];
+                $scope.toDataUrl(results[i],function(base64img){
+                  //console.log(base64img);
+                  $scope.data.image = base64img;
+                  $scope.$digest();
+                },'png');
+                //console.log(results[i]);
+                //$scope.image=results[i];
               }
             }, function(error) {
               // error getting photos
             });
         });
   }
+  $scope.toDataUrl = function(src, callback, outputFormat) {
+      var img = new Image();
+      img.crossOrigin = 'Anonymous';
+      img.onload = function() {
+        var canvas = document.createElement('CANVAS');
+        var ctx = canvas.getContext('2d');
+        var dataURL;
+        canvas.height = this.height;
+        canvas.width = this.width;
+        ctx.drawImage(this, 0, 0);
+        dataURL = canvas.toDataURL(outputFormat);
+        callback(dataURL);
+      };
+      img.src = src;
+      if (img.complete || img.complete === undefined) {
+        img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+        img.src = src;
+      }
+    }
 
 })
 
